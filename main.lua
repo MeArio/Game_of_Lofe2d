a76348e2100449c6affb482c7386e71eabf4d7ba
MeArio <dorindovleac@gmail.com>
2017-07-28 03:22:41 +0300

    nothing much just some basic argument parsing

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+local argparse = require 'libs.argparse'
+local parser = argparse()
+parser:argument("dir", "love dir") -- This is here because the parser freaks out because of the love command folder path
+parser:option("-s --scale", "Game scale", 10)
+parser:option("-w --width", "Game width", 32)
+parser:option("-h --height", "Game height", 24)
+local args = parser:parse()
+
 -- luacheck: globals love
 function love.load()
 
